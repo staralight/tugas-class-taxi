db34ecc66f9885e1c3fae30526a149a01b63ba0c
tarourskyy <pchytara@gmail.com>
2021-03-17 15:54:44 +0700

    menambahkan project

---
+++ ClassTaxi/Taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName: {0}", DriverName);
+            if (OnDuty) Console.WriteLine("OnDuty: Yes");
+            else Console.WriteLine("OnDuty: No");
+            Console.WriteLine("NumPassenger: {0}\n", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
